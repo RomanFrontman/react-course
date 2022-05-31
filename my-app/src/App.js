f1ed623ccc6c7be928ed3d82654da817150bd397
Roman Chaus <roman1997lviv@gmail.com>
2022-05-31 12:38:35 +0300

    feedback text and rating output added, the number of files is optimized

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,64 @@
 import './App.css';
+import React, { useState } from "react";
 import { Header } from './Header.js';
-import { Content } from './Content.js';
 import { Footer } from './Footer.js';
-import { Feedback } from './Feedback.js';
+import { Button } from './Button.js';
+import { FeedbackList } from "./FeedbackList.js";
+import { List } from "./List.js";
 
-const studentsName = "Roman Chaus"
 
-function App() {
+
+export default function App() {
+  const studentsName = "Roman Chaus"
+  const [feedbackText, setFeedbackText] = useState("");
+  const [rating, setRating] = useState(10);
+  const [feedbackList, setFeedbackList] = useState([]);
+
+const handleInputChange = (event) => {
+  const value = event.target.value;
+  setFeedbackText(value);
+};
+
+const onRatingChange = (event) => {
+  const value = +event.currentTarget.value;
+  setRating(value);
+};
+
+const onButtonClick = () => {
+  setFeedbackList([
+    ...feedbackList,
+    { rate: rating, rateText: feedbackText }
+  ]);
+  setRating(10);
+  setFeedbackText("");
+};
+
   return (
     <div className="App">
-
-        <Header name={studentsName}/>
-        <Content/>
-        <Feedback></Feedback>
-        <Footer name={studentsName}/>
-
-
+      <Header name={studentsName}/>
+      <div>
+        <h2>How would you rate our course?</h2>
+      </div>
+      <List
+          listLength={10}
+          rating={rating}
+          onRatingChange={onRatingChange}
+        />
+      <div className="controlling">
+        <input
+          type="text"
+          name="textInput"
+          placeholder='Write your text'
+          value={feedbackText}
+          onChange={handleInputChange}
+        />
+        <Button disabled={!feedbackText} onClick={onButtonClick} 
+        />
+      </div>
+      <br />
+      <FeedbackList list={feedbackList} />
+      <br />
+      <Footer name={studentsName}/>
     </div>
   );
 }
-
-export default App;
--- my-app/src/List.js
+++ my-app/src/List.js
@@ -1,12 +1,19 @@
 import React from "react";
 
-export const List = ({listLength = 10}) => {
+export const List = ({listLength = 10, rating, onRatingChange }) => {
     return (
         <ul>
             {Array.from({ length: listLength }, (_, i) => {
                 return (
                 <li>
-                    <input type="radio" name="rating" id={`num${i + 1}`} />
+                    <input 
+                    type="radio" 
+                    name="rating" 
+                    id={`num${i + 1}`}
+                    checked={rating === i + 1}
+                    onChange={onRatingChange}
+                    value={i + 1} 
+                    />
                     <label htmlFor={`num${i + 1}`}>{i + 1}</label>
                 </li>
                 );
